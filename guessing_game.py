29466dbe252d245c85970feaa1e2508b0de6935a
Derryn Edwards <derryn_93@live.com>
2018-11-27 14:30:05 -0600

    add pep8 guidelines and clean code

--- guessing_game.py
+++ guessing_game.py
@@ -1,20 +1,12 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
+
 
 def get_prompt():
     """
-    Prompts the user to input a number from 1-20, and validates if the input is proper or not.
-    If the input is not correct, it will prompt the user to try again. Returns the validated number.
+    Prompts the user to input a number from 1-20,
+    and validates if the input is proper or not.
+    If the input is not correct, it will prompt the user to try again.
+    Returns the validated number.
     """
     while True:
         prompt = input("Please enter a number from 1-20: ")
@@ -30,8 +22,9 @@ def get_prompt():
 
 def guess_number(number):
     """
-    Receives a number and asks the user to try and guess the number. Once guessed,
-    it returns the number of tries it took the user to guess the number.
+    Receives a number and asks the user to try and guess the number.
+    Once guessed, it returns the number of tries it took the user to
+    guess the number.
     """
     tries = 0
     number = number
@@ -63,21 +56,8 @@ def generate_number():
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
+    """
+    Starts the Number Guessing Game.
     """
     print("""
 ====================================
@@ -85,11 +65,11 @@ Welcome to the Number Guessing Game!
       Good Luck and Have Fun!
 ====================================
 """)
-    #set a random number from 1 - 20 inclusive.
+
     high_score = 0
     current_score = 0
     number = generate_number()
-    
+
     while True:
         tries = guess_number(number)
         current_score = tries
@@ -99,9 +79,9 @@ Welcome to the Number Guessing Game!
             high_score = current_score
         elif current_score < high_score:
             high_score = current_score
-  
-        while True: 
-            play_again = input("Would you like to play again? Yes (y) / No  (n)? ")
+
+        while True:
+            play_again = input("Would you like to play again? Yes(y)/No(n)? ")
 
             if play_again == 'y':
                 print("Generating new number...")
@@ -110,15 +90,13 @@ Welcome to the Number Guessing Game!
                 break
             elif play_again == 'n':
                 print("Thanks for playing! Goodbye!")
-                break           
+                break
             else:
                 print("Wrong input. Try again!")
                 continue
 
         if play_again == 'n':
             break
-                
-        
 
 
 if __name__ == '__main__':
